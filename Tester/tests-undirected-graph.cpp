260ad2393ce23fa8ed32d97f61c5e06dd7243036
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2021-07-01 17:03:02 -0500

    test(UndirectedGraph): Added tests for checking edge insertion

--- Tester/tests-undirected-graph.cpp
+++ Tester/tests-undirected-graph.cpp
@@ -25,3 +25,38 @@ TEST_CASE( "Inserting vertices in an undirected graph", "[UnDirectedGraph]" ) {
 	
     }
 }
+
+TEST_CASE( "Inserting edges in an undirected graph", "[UnDirectedGraph]" ) {
+  
+    SECTION( "Minimal tests" ) {
+    
+        SECTION( "No edges" ) {
+            UnDirectedGraph<double, int> g = UnDirectedGraph<double, int>();
+            REQUIRE( g.getNumberOfEdges() == 0 );
+        }
+    
+        SECTION( "1 edge" ) {
+            UnDirectedGraph<double, int> g = UnDirectedGraph<double, int>();
+
+            g.insertVertex("A", 1);
+            g.insertVertex("B", 1);
+
+            g.createEdge("A", "B", 1);
+            
+            REQUIRE( g.getNumberOfEdges() == 1 );
+        }
+
+        SECTION( "10 edges" ) {
+            UnDirectedGraph<double, int> g = UnDirectedGraph<double, int>();
+
+            g.insertVertex("A", 1);
+            g.insertVertex("B", 1);
+
+            for(int i=0; i<10; i++)
+                g.createEdge("A", "B", 1);
+            
+            REQUIRE( g.getNumberOfEdges() == 10 );
+        }
+        
+    }
+}
