42ee3efe9e8fbaf5bdb7ab427cc7ff241f52a0cc
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2021-07-01 15:25:44 -0500

    Fixed style in tests

--- Tester/tests-undirected-graph.cpp
+++ Tester/tests-undirected-graph.cpp
@@ -2,34 +2,26 @@
 #include "UndirectedGraph.h"
 
 TEST_CASE( "Inserting vertices in an undirected graph", "[UnDirectedGraph]" ) {
-
-  SECTION( "Minimal tests" ) {
-
-    SECTION( "No vertices" ){
-
-      UnDirectedGraph<double, int> g = UnDirectedGraph<double, int>();
-      REQUIRE( g.getNumberOfVertices() == 0 );
-      
-    }
-    
-    SECTION( "A single vertex" ){
-
-      UnDirectedGraph<double, int> g = UnDirectedGraph<double, int>();
-      g.insertVertex("A", 1);
-      REQUIRE( g.getNumberOfVertices() == 1 );
-      
-    }
-    
-    SECTION( "All characters in the alphabet" ){
-      
-      UnDirectedGraph<double, int> g = UnDirectedGraph<double, int>();
-      for(char i='a'; i<='z'; ++i)
-	g.insertVertex(i, 10);
-      
-      REQUIRE( g.getNumberOfVertices() == ('z' - 'a' + 1) );
-      
-    }
-    
-  }
   
+    SECTION( "Minimal tests" ) {
+    
+	SECTION( "No vertices" ) {
+	    UnDirectedGraph<double, int> g = UnDirectedGraph<double, int>();
+	    REQUIRE( g.getNumberOfVertices() == 0 );
+	}
+    
+	SECTION( "A single vertex" ) {
+	    UnDirectedGraph<double, int> g = UnDirectedGraph<double, int>();
+	    g.insertVertex("A", 1);
+	    REQUIRE( g.getNumberOfVertices() == 1 );
+	}
+	
+	SECTION( "All characters in the alphabet" ) {
+	    UnDirectedGraph<double, int> g = UnDirectedGraph<double, int>();
+	    for(char i='a'; i<='z'; ++i)
+		g.insertVertex(i, 10);
+	    REQUIRE( g.getNumberOfVertices() == ('z' - 'a' + 1) );
+	}
+	
+    }
 }
