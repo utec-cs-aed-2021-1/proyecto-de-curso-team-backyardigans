d2170ae283c1b566628ddad3d4c7c1daa9afb36d
rmg-90 <rodrigo.morales@utec.edu.pe>
2021-07-02 01:57:54 -0500

    test(DirectedGraph): Test for inserting vertices added

--- Tester/DirectedGraph.cpp
+++ Tester/DirectedGraph.cpp
@@ -1,6 +1,14 @@
 #include "catch.hpp"
 #include "DirectedGraph.h"
 
-TEST_CASE( "This is a simple test" ) {
-    REQUIRE( 1 == 1 );
+TEST_CASE( "Inserting vertices in a directed graph", "[DirectedGraph]" ) {
+  
+    SECTION( "Minimal tests" ) {
+        DirectedGraph<double, int> g = DirectedGraph<double, int>();
+        
+        SECTION( "No vertices" ) {
+            REQUIRE( g.getNumberOfVertices() == 0 );
+        }
+        
+    }
 }
