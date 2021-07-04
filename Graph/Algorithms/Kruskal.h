e29be64ef89cfcea9c8dfc33ed645e97a05ed652
paulozterra <66433825+paulozterra@users.noreply.github.com>
2021-07-04 12:14:44 -0500

    caia iamax3

--- Graph/Algorithms/Kruskal.h
+++ Graph/Algorithms/Kruskal.h
@@ -4,6 +4,7 @@
 #include "UndirectedGraph.h"
 #include "helpclass/priorityqueue.h"
 #include "helpclass/dsarray.h"
+#include "vector"
 
 template<typename TV, typename TE>
 class Kruskal{
@@ -11,36 +12,52 @@ class Kruskal{
 protected:
 
     UnDirectedGraph<TV, TE>* graph;
-
+    vector<TE> v;
 public:
     Kruskal();
-    Kruskal(UnDirectedGraph<TV, TE>* graph_){
+    Kruskal(UnDirectedGraph<TV, TE>* graph_,vector<TE> v_){
         graph = graph_;
+        v=v_;
     }
 
 
     UnDirectedGraph<TV, TE>* apply(){
         auto* Ud_1= new UnDirectedGraph<TV, TE>();
         priority_<TE> cola;
-        auto map1 = graph->getMap();
+        auto map1 = graph->getMap(); // map vert
         unordered_map<string, bool> map2;
+        unordered_map<string, int> mapx;//map pos
+        int aux= 0;
         for(auto it=map1.begin();it!=map1.end();it++){
-            for(auto ite=(*it.second)->edges.begin();ite!=(*it.second)->edges.end();ite++){
-                string iu1 =(*ite)->vertexes[0]->id;
-                string iu2 =(*ite)->vertexes[1]->id;
-                if(map2.find(iu1+iu2)==map2.end() || map2.find(iu2+iu1)==map2.end()){
-                    pair<TE,string>p={(*ite)->weigth,iu1+iu2};
-                    map2[iu1+iu2]=true;
+            mapx[it->first]=aux++;
+            v.push_back(it->first);
+            Ud_1->insertVertex(it->first,map1[it->first]->data);
+            for(auto ite=(*it.second)->edges.begin();ite!=(*it.second)->edges.end();ite++) {
+                string iu1 = (*ite)->vertexes[0]->id;
+                string iu2 = (*ite)->vertexes[1]->id;
+                if (map2.find(iu1 + iu2) == map2.end() || map2.find(iu2 + iu1) == map2.end()) {
+                    pair<TE, string> p = {(*ite)->weigth, iu1 + iu2};
+                    map2[iu1 + iu2] = true;
                     cola.push(p);
                 }
+
             }
         }
-        for(auto it=map1.begin();it!=map1.end();it++){
 
+        auto d = new DisjoinSet<TE>(v);
+        d->Makeset();
+        while(!cola.is_empty()){
+            string u=cola.top().second;
+
+            string u1= to_string(u[0]);
+            string u2=to_string(u[1]);
+
+            if(d->Find(mapx[u1])!=d->Find(mapx[u2])){
+                d->Union(mapx[u1],mapx[u2]);
+                Ud_1->createEdge(u1,u2,cola.top().first);
+            }
+            cola.pop();
         }
-
-
-
 
         return Ud_1;
     }
