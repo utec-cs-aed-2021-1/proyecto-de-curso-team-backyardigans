a62f81ac76e9d0803a0ee1b4e1284be3cb6f9628
rmg-90 <rodrigo.morales@utec.edu.pe>
2021-07-05 02:15:31 -0500

    test(DFS, BFS): Fix styles of tests

--- Tester/BFS.cpp
+++ Tester/BFS.cpp
@@ -1,43 +1,52 @@
-
 #include "catch.hpp"
 #include "Algorithms/BFS.h"
 #include "UndirectedGraph.h"
 
-TEST_CASE( "A simple test for BFS" ) {
-    SECTION( "Edges creation" ) {
-        Graph<double, int>* g1 = new UnDirectedGraph<double, int>();
-        g1->insertVertex("A", 1);
-        g1->insertVertex("B", 1);
-        g1->insertVertex("C", 1);
-        g1->insertVertex("D", 1);
-        g1->insertVertex("E", 1);
-        g1->insertVertex("F", 1);
-        g1->createEdge("A", "B", 2);
-        g1->createEdge("A", "C", 2);
-        g1->createEdge("A", "D", 2);
-        g1->createEdge("D", "E", 2);
-        g1->createEdge("B", "F", 2);
-        g1->createEdge("F", "C", 2);
-        g1->createEdge("C", "E", 2);
-        BFS<double ,int> bfs(g1, "A");
-        DirectedGraph<double , int>* gr = bfs.apply();
-        cout << "BFS: "<<endl;
-        g1->display();
-    }
-    SECTION( "Edges creation_2" ) {
-        Graph<double, int>* g1 = new UnDirectedGraph<double, int>();
-        g1->insertVertex("A", 1);
-        g1->insertVertex("B", 1);
-        g1->insertVertex("C", 1);
-        g1->insertVertex("D", 1);
+TEST_CASE( "BFS", "[BFS]" ) {
+    SECTION( "Minimal working examples" ) {
+        SECTION( "Example 1" ) {
+            Graph<double, int>* g1 = new UnDirectedGraph<double, int>();
+            
+            g1->insertVertex("A", 1);
+            g1->insertVertex("B", 1);
+            g1->insertVertex("C", 1);
+            g1->insertVertex("D", 1);
+            g1->insertVertex("E", 1);
+            g1->insertVertex("F", 1);
+            
+            g1->createEdge("A", "B", 2);
+            g1->createEdge("A", "C", 2);
+            g1->createEdge("A", "D", 2);
+            g1->createEdge("D", "E", 2);
+            g1->createEdge("B", "F", 2);
+            g1->createEdge("F", "C", 2);
+            g1->createEdge("C", "E", 2);
+            
+            BFS<double ,int> bfs(g1, "A");
+            DirectedGraph<double , int>* gr = bfs.apply();
+            
+            cout << "BFS: "<<endl;
+            g1->display();
+        }
+    
+        SECTION( "Example 2" ) {
+            Graph<double, int>* g1 = new UnDirectedGraph<double, int>();
+            g1->insertVertex("A", 1);
+            g1->insertVertex("B", 1);
+            g1->insertVertex("C", 1);
+            g1->insertVertex("D", 1);
 
-        g1->createEdge("A", "C", 4);
-        g1->createEdge("C", "D", 3);
-        g1->createEdge("A", "D", 10);
+            g1->createEdge("A", "C", 4);
+            g1->createEdge("C", "D", 3);
+            g1->createEdge("A", "D", 10);
 
-        BFS<double ,int> bfs(g1, "C");
-        DirectedGraph<double , int>* gr = bfs.apply();
-        cout << "BFS: "<<endl;
-        g1->display();
+            BFS<double ,int> bfs(g1, "C");
+            DirectedGraph<double , int>* gr = bfs.apply();
+            
+            cout << "BFS: "<<endl;
+            g1->display();
+        }
+
     }
+    
 }
--- Tester/DFS.cpp
+++ Tester/DFS.cpp
@@ -1,42 +1,51 @@
 #include "catch.hpp"
 #include "Algorithms/DFS.h"
 
-TEST_CASE( "A simple test for DFS" ) {
-    SECTION( "Edges creation" ) {
-        Graph<double, int>* g1 = new UnDirectedGraph<double, int>();
-        g1->insertVertex("A", 1);
-        g1->insertVertex("B", 1);
-        g1->insertVertex("C", 1);
-        g1->insertVertex("D", 1);
-        g1->insertVertex("E", 1);
-        g1->insertVertex("F", 1);
-        g1->createEdge("A", "B", 2);
-        g1->createEdge("A", "C", 2);
-        g1->createEdge("A", "D", 2);
-        g1->createEdge("D", "E", 2);
-        g1->createEdge("B", "F", 2);
-        g1->createEdge("F", "C", 2);
-        g1->createEdge("C", "E", 2);
-        DFS<double ,int> dfs(g1, "A");
-        DirectedGraph<double , int>* gr = dfs.apply();
-        cout << "BFS: "<<endl;
-        g1->display();
-    }
-    SECTION( "Edges creation_2" ) {
-        Graph<double, int>* g1 = new UnDirectedGraph<double, int>();
-        g1->insertVertex("A", 1);
-        g1->insertVertex("B", 1);
-        g1->insertVertex("C", 1);
-        g1->insertVertex("D", 1);
+TEST_CASE( "DFS", "[DFS]" ) {
+    SECTION( "Minimal working examples" ) {
+        SECTION( "Example 1" ) {
+            Graph<double, int>* g1 = new UnDirectedGraph<double, int>();
+            
+            g1->insertVertex("A", 1);
+            g1->insertVertex("B", 1);
+            g1->insertVertex("C", 1);
+            g1->insertVertex("D", 1);
+            g1->insertVertex("E", 1);
+            g1->insertVertex("F", 1);
+            
+            g1->createEdge("A", "B", 2);
+            g1->createEdge("A", "C", 2);
+            g1->createEdge("A", "D", 2);
+            g1->createEdge("D", "E", 2);
+            g1->createEdge("B", "F", 2);
+            g1->createEdge("F", "C", 2);
+            g1->createEdge("C", "E", 2);
+            
+            DFS<double ,int> dfs(g1, "A");
+            
+            DirectedGraph<double , int>* gr = dfs.apply();
+            
+            cout << "BFS: "<<endl;
+            g1->display();
+        }
+        SECTION( "Example 2" ) {
+            Graph<double, int>* g1 = new UnDirectedGraph<double, int>();
+            
+            g1->insertVertex("A", 1);
+            g1->insertVertex("B", 1);
+            g1->insertVertex("C", 1);
+            g1->insertVertex("D", 1);
 
-        g1->createEdge("A", "C", 4);
-        g1->createEdge("C", "D", 3);
-        g1->createEdge("A", "D", 10);
+            g1->createEdge("A", "C", 4);
+            g1->createEdge("C", "D", 3);
+            g1->createEdge("A", "D", 10);
 
-        DFS<double ,int> dfs(g1, "C");
-        DirectedGraph<double , int>* gr = dfs.apply();
-        cout << "BFS: "<<endl;
-        g1->display();
+            DFS<double ,int> dfs(g1, "C");
+            DirectedGraph<double , int>* gr = dfs.apply();
+            
+            cout << "BFS: "<<endl;
+            g1->display();
+        }
     }
 }
 
